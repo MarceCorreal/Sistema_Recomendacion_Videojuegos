4b3e4dbfbc003ff73bcfcfc4fd48f7048b5667fa
Marce Correal <marcecor@hotmail.com>
2024-03-11 22:20:05 -0500

    subiendo main

--- main.py
+++ main.py
@@ -51,3 +51,44 @@ async def get_developer(desarrollador: str):
         return {'error': 'Ocurrió un error al procesar la solicitud'}
 
 
+# Función Userdata________________________________________________________________________________________________________________________________________________________________________________
+
+def userdata(df_userdata, user_id):
+    
+    # Filtrar df por desarrollador developer
+    
+    df_developer_filtrado = df_developer[df_developer['developer'] == desarrollador]
+    
+    # Producir resultado developer
+    
+    respuestas_anuales  = df_developer_filtrado.groupby('release_year').agg({
+        'items_free':'sum', 
+        'items_total':'sum',
+        'porcentaje': lambda x:(pd.to_numeric(x.str.rstrip('%'), errors='coerce') /  100).mean() *100
+        }).reset_index()
+    
+    # Definir respuestas developer
+    respuestas_anuales['porcentaje'] = respuestas_anuales['porcentaje'].round(2)
+    respuestas_anuales = respuestas_anuales.rename(columns={'release_year': 'Anio'})
+    respuestas_anuales = respuestas_anuales.rename(columns={'items_total': 'Items'})
+    respuestas_anuales = respuestas_anuales.rename(columns={'porcentaje': '%Free'})
+    return respuestas_anuales[['Anio', 'Items', '%Free']]
+
+# Definir ruta - Decorador_____________________________________________________________________________________________________________________________________
+
+@app.get("/developer/{desarrollador}")
+
+async def get_developer(desarrollador: str):
+
+    
+# Ejecución de la Función developer
+    
+    try:
+        # Ruta_df_developer = r"C:\Users\Usuario\Henry\PI1_ML\Funciones\Developer\df_developer.parquet"
+        ruta_df_developer = r"C:\Users\Usuario\Henry\PI1_MachineLearning\Funciones\Developer\df_developer.parquet"  #"Funciones/Developer/df_developer.parquet"
+        df_developer = pd.read_parquet(ruta_df_developer)
+        result = developer(df_developer, desarrollador)
+        return result.to_dict(orient="records")
+    except Exception as e:
+        print(e)  # Imprimir el error para depuración
+        return {'error': 'Ocurrió un error al procesar la solicitud'}
