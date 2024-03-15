b3e389114fcc53073b10cbb0db8ded8f680d12c4
MaceCorreal <marcecor@hotmail.com>
2024-03-15 11:21:37 -0500

    Actualizando el main con funciones

--- main.py
+++ main.py
@@ -14,66 +14,57 @@ from scipy.sparse import csr_matrix
 app = FastAPI()
 
 
-# Developer________________________________________________________________________________________________________________________________________________________________________________
-    
-def developer(dataframe, desarrollador):
+# developer_reviews_analysis________________________________________________________________________________________________________________________________________________________________________________
+
+def developer_reviews_analysis(df, desarrolladora):
 
     '''
-    Esta función devuelve estadísticas por año para un desarrollador específico en un DataFrame.
-        
+    Esta función realiza un análisis de sentimientos de las reseñas para una desarrolladora específica.
+         
     Args:
-        dataframe (pd.DataFrame): DataFrame que contiene los datos.
-        desarrollador (str): Nombre del desarrollador a analizar.
+        df (pd.DataFrame): DataFrame que contiene los datos.
+        desarrolladora (str): Nombre de la desarrolladora a analizar.
     
     Returns:
-        pd.DataFrame: Un DataFrame que contiene estadísticas por año para el desarrollador.
-            - 'Año' (int): Año de lanzamiento.
-            - 'Items' (int): Cantidad total de items lanzados por el desarrollador.
-            - '% Free' (float): Porcentaje medio de items gratuitos lanzados por el desarrollador.
+        dict: Un diccionario que contiene el resultado del análisis de sentimientos.
+            - La clave es el nombre de la desarrolladora.
+            - El valor es una lista con la cantidad de reseñas negativas y positivas.
     '''
 
-    df_desarrollador = dataframe[dataframe['developer'] == desarrollador]
-    stats_por_anio = df_desarrollador.groupby('release_anio').agg({
-        'items_free': 'sum',
-        'items_total': 'sum',
-        'percentage_free': lambda x: (pd.to_numeric(x.str.rstrip('%'), errors='coerce') / 100).mean() * 100
-    }).reset_index()
-    stats_por_anio['percentage_free'] = stats_por_anio['percentage_free'].round(2)
-    stats_por_anio = stats_por_anio.rename(columns={'release_anio': 'Año'})
-    stats_por_anio = stats_por_anio.rename(columns={'items_total': 'Items'})
-    stats_por_anio = stats_por_anio.rename(columns={'percentage_free': '% Free'})
-    return stats_por_anio[['Año', 'Items', '% Free']]
+    filtered_data = df[df['developer'] == desarrolladora]
+    positive_count = 0
+    negative_count = 0
 
-@app.get("/developer/{desarrollador}")
+    for sentiment in filtered_data['sentiment_analysis']:
+        if sentiment == 0:
+            negative_count += 1
+        elif sentiment == 2:
+            positive_count += 1
 
-async def get_developer(desarrollador: str):
+    result = {desarrolladora: [f"Negative = {negative_count}", f"Positive = {positive_count}"]}
+    return result
+
+@app.get("/developer_reviews_analysis/{desarrolladora}",
+                    description=
+                    """ <font color="black">
+                    INSTRUCCIONES<br>
+                    1. Haga clik en "Try it out".<br>
+                    2. Ingrese el developer en el box abajo. Ejemplo: Valve <br>
+                    3. Scrollear a "Response body" para ver cuantas reseñas positivas y negativas tuvo el desarrollador.
+                    </font>"""
+                    , tags=['Consultas PI1-ML'])
+async def get_developer_reviews_analysis(desarrolladora: str):
     try:
-        #parquet_path = "C:\Users\Usuario\Henry\Nuevo_proyecto\Funciones\developer\dataset_endpoint_1.parquet"
-        parquet_path = r"C:\Users\Usuario\Henry\Nuevo_proyecto\Funciones\developer\dataset_endpoint_1.parquet"
-        df = pd.read_parquet(parquet_path)
-        result = developer(df, desarrollador)
-        return result.to_dict(orient="records")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar la solicitud: {str(e)}")
-
-
-@app.get("/developer/{desarrollador}")
-async def get_desarrollador(desarrollador: str):
-    try:
-        parquet_path1 = "dataset_endpoint_1.parquet"
-        #parquet_path1 =  "C:\Users\Usuario\Henry\Nuevo_proyecto\Funciones\developer\dataset_endpoint_1.parquet"
-        
-        df = pd.read_parquet(parquet_path1)
-        
-        # Llama a la función userdata
-        result = userdata(df, desarrllador)
+        parquet_path5 = r"C:\Users\Usuario\Henry\PI1_ML\Funciones\developer_reviews_analysis\dataset_endpoint_5.parquet"
+        #parquet_path5 = "C:\Users\Usuario\Henry\PI1_ML\Funciones\developer_reviews_analysis\dataset_endpoint_5.parquet"
+        df = pd.read_parquet(parquet_path5)
+        result = developer_reviews_analysis(df, desarrolladora)
         return JSONResponse(content=result)
     except Exception as e:
-        print(e)  # Imprimir el error para depuración
-        return {'error': 'Ocurrió un error al procesar la solicitud'}
-    
-    
-    
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+
 # Userdata________________________________________________________________________________________________________________________________________________________________________________
 
 def userdata(df, user_id):
@@ -94,9 +85,6 @@ def userdata(df, user_id):
     '''
 
     usuario = df[df['user_id'] == user_id]
-
-    if usuario.empty:
-        raise HTTPException(status_code=404, detail=f"El usuario {user_id} no existe en el DataFrame.")
 
     dinero_gastado = usuario['cantidad total gastado'].iloc[0]
     porcentaje_recomendacion = float(usuario['percentage_true'].iloc[0].rstrip('%'))
@@ -119,7 +107,7 @@ def userdata(df, user_id):
                     2. Ingrese un identificador de usuario (user_id) para Debe devolver cantidad de dinero gastado por el usuario, el porcentaje de recomendación en base a reviews.recommend y cantidad de items.. Ejemplo: GamekungX <br>
                     3. Scrollear a "Response body" para ver el resultado.
                     </font>"""
-                    , tags=['Consultas generales'])
+                    , tags=['Consultas PI1-ML'])
 async def get_user_id(user_id: str):
     try:
         parquet_path2 = "Dataset/dataset_endpoint_2.parquet"
@@ -170,7 +158,7 @@ def UserForGenre(genre: str, df):
                     2. Ingrese un genero para devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año de lanzamiento. Ejemplo: Action <br>
                     3. Scrollear a "Response body" para ver el resultado.
                     </font>"""
-                    , tags=['Consultas generales'])
+                    , tags=['Consultas PI1-ML'])
 async def get_user_for_genre(genre: str):
     try:
         parquet_path3 = r"C:\Users\Usuario\Henry\PI1_ML\Funciones\userforgenre\dataset_endpoint_3.parquet"
@@ -215,7 +203,7 @@ def best_developer_year(dataframe, year):
                     2. Ingrese un año para saber el top 3 de desarrolladores con juegos MÁS recomendados por usuarios para el año dado.. Ejemplo: 2015 <br>
                     3. Scrollear a "Response body" para ver el resultado.
                     </font>"""
-                    , tags=['Consultas generales'])
+                    , tags=['Consultas PI1-ML'])
 async def get_best_developer_year(year: int):
     try:
         parquet_path4 = r"Funciones\best_developer_year\dataset_endpoint_4.parquet"
@@ -225,53 +213,56 @@ async def get_best_developer_year(year: int):
         return JSONResponse(content=result)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
-
-
-# developer_reviews_analysis________________________________________________________________________________________________________________________________________________________________________________
-
-def developer_reviews_analysis(df, desarrolladora):
+    
+    
+    
+# Developer________________________________________________________________________________________________________________________________________________________________________________
+    
+def developer(dataframe, desarrollador):
 
     '''
-    Esta función realiza un análisis de sentimientos de las reseñas para una desarrolladora específica.
+    Esta función devuelve estadísticas por año para un desarrollador específico en un DataFrame.
          
     Args:
-        df (pd.DataFrame): DataFrame que contiene los datos.
-        desarrolladora (str): Nombre de la desarrolladora a analizar.
+        dataframe (pd.DataFrame): DataFrame que contiene los datos.
+        desarrollador (str): Nombre del desarrollador a analizar.
     
     Returns:
-        dict: Un diccionario que contiene el resultado del análisis de sentimientos.
-            - La clave es el nombre de la desarrolladora.
-            - El valor es una lista con la cantidad de reseñas negativas y positivas.
+        pd.DataFrame: Un DataFrame que contiene estadísticas por año para el desarrollador.
+            - 'Año' (int): Año de lanzamiento.
+            - 'Items' (int): Cantidad total de items lanzados por el desarrollador.
+            - '% Free' (float): Porcentaje medio de items gratuitos lanzados por el desarrollador.
     '''
 
-    filtered_data = df[df['developer'] == desarrolladora]
-    positive_count = 0
-    negative_count = 0
+    df_desarrollador = dataframe[dataframe['developer'] == desarrollador]
+    stats_por_anio = df_desarrollador.groupby('release_anio').agg({
+        'items_free': 'sum',
+        'items_total': 'sum',
+        'percentage_free': lambda x: (pd.to_numeric(x.str.rstrip('%'), errors='coerce') / 100).mean() * 100
+    }).reset_index()
+    stats_por_anio['percentage_free'] = stats_por_anio['percentage_free'].round(2)
+    stats_por_anio = stats_por_anio.rename(columns={'release_anio': 'Año'})
+    stats_por_anio = stats_por_anio.rename(columns={'items_total': 'Items'})
+    stats_por_anio = stats_por_anio.rename(columns={'percentage_free': '% Free'})
+    return stats_por_anio[['Año', 'Items', '% Free']]
 
-    for sentiment in filtered_data['sentiment_analysis']:
-        if sentiment == 0:
-            negative_count += 1
-        elif sentiment == 2:
-            positive_count += 1
-
-    result = {desarrolladora: [f"Negative = {negative_count}", f"Positive = {positive_count}"]}
-    return result
-
-@app.get("/developer_reviews_analysis/{desarrolladora}",
+@app.get("/developer/{desarrollador}",
                     description=
                     """ <font color="black">
                     INSTRUCCIONES<br>
                     1. Haga clik en "Try it out".<br>
-                    2. Ingrese el developer en el box abajo. Ejemplo: Valve <br>
-                    3. Scrollear a "Response body" para ver cuantas reseñas positivas y negativas tuvo el desarrollador.
+                    2. Ingrese un desarrollador para ver la cantidad de items y porcentaje de contenido Free por año según empresa desarrolladora. Ejemplo: Valve <br>
+                    3. Scrollear a "Response body" para ver el resultado.
                     </font>"""
-                    , tags=['Consultas generales'])
-async def get_developer_reviews_analysis(desarrolladora: str):
+                    , tags=['Consultas PI1-ML'])
+async def get_developer(desarrollador: str):
     try:
-        parquet_path5 = r"C:\Users\Usuario\Henry\PI1_ML\Funciones\developer_reviews_analysis\dataset_endpoint_5.parquet"
-        #parquet_path5 = "C:\Users\Usuario\Henry\PI1_ML\Funciones\developer_reviews_analysis\dataset_endpoint_5.parquet"
-        df = pd.read_parquet(parquet_path5)
-        result = developer_reviews_analysis(df, desarrolladora)
-        return JSONResponse(content=result)
+        #parquet_path = "C:\Users\Usuario\Henry\PI1_ML\Funciones\developer\dataset_endpoint_1.parquet"
+        parquet_path = r"C:\Users\Usuario\Henry\PI1_ML\Funciones\developer\dataset_endpoint_1.parquet"
+        df = pd.read_parquet(parquet_path)
+        result = developer(df, desarrollador)
+        return result.to_dict(orient="records")
     except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        raise HTTPException(status_code=500, detail=f"Error al procesar la solicitud: {str(e)}")
+
+
